41253bfbc96a71726fc41b131ba5c5305c4e914b
Zidni Rif'an <Zidni Rif'an>
2021-07-14 16:21:56 +0700

    Skills section

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,15 @@ navClose.addEventListener('click', closeMenu);
 const navLink = document.querySelectorAll('.nav-link');
 
 navLink.forEach((e) => e.addEventListener('click', closeMenu));
+
+const skillsHeader = document.querySelectorAll('.skills-header');
+
+const toggleSkills = (e) => {
+  if (e.target.classList.contains('skills-header')) {
+    e.target.parentElement.classList.toggle('skills-open');
+  } else {
+    e.target.parentElement.parentElement.classList.toggle('skills-open');
+  }
+};
+
+skillsHeader.forEach((e) => e.addEventListener('click', toggleSkills));
