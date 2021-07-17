dd780632f93260b6595ad7d67aa1793a4928f296
Zidni Rif'an <Zidni Rif'an>
2021-07-17 12:21:13 +0700

    active link

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,3 +47,29 @@ const swiper = [
     },
   }),
 ];
+
+// Active Link
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav-menu a[href*=${sectionId}]`)
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector(`.nav-menu a[href*=${sectionId}]`)
+        .classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
